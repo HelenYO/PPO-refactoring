6d3f93fa7b9c46c303e66c56cda43bab699de97f
Lena Ilina <ferr.98@mail.ru>
2021-01-04 18:47:41 +0300

    db function query

--- src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -15,88 +15,83 @@ import java.sql.Statement;
  * @author akirakozov
  */
 public class QueryServlet extends HttpServlet {
+
+    private String dbFuncSumCount(String command) {
+        String responseBody = "";
+        String header = "";
+        String dbCommand = "";
+        if (command.equals("sum")) {
+            dbCommand = "SELECT SUM(price) FROM PRODUCT";
+            header = "Summary price: \n";
+        } else if (command.equals("count")) {
+            dbCommand = "SELECT COUNT(*) FROM PRODUCT";
+            header = "Number of products: \n";
+        }
+        try {
+            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+                Statement stmt = c.createStatement();
+                ResultSet rs = stmt.executeQuery(dbCommand);
+                String temp = "";
+                if (rs.next()) {
+                    temp = ("" + rs.getInt(1));
+                }
+                responseBody = ResponseMaker.funcResponse(header, Boolean.FALSE, temp);
+                rs.close();
+                stmt.close();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return responseBody;
+    }
+
+    private String dbFuncMinMax(String command) {
+        String dbCommand = "";
+        if (command.equals("min")) {
+            dbCommand = "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1";
+        } else if (command.equals("max")) {
+            dbCommand = "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1";
+        }
+        String responseBody = "";
+        try {
+            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+                Statement stmt = c.createStatement();
+                ResultSet rs = stmt.executeQuery(dbCommand);
+                StringBuilder temp = new StringBuilder();
+                while (rs.next()) {
+                    String name = rs.getString("name");
+                    int price = rs.getInt("price");
+                    temp.append(name).append("\t").append(price).append("</br>");
+                }
+                String header = "Product with " + command + " price: ";
+                responseBody = ResponseMaker.funcResponse(header, Boolean.TRUE, temp.toString());
+                rs.close();
+                stmt.close();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return responseBody;
+    }
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String command = request.getParameter("command");
         String responseBody = "";
 
         if ("max".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-                    StringBuilder temp = new StringBuilder();
-                    while (rs.next()) {
-                        String name = rs.getString("name");
-                        int price = rs.getInt("price");
-                        temp.append(name).append("\t").append(price).append("</br>");
-                    }
-                    responseBody = ResponseMaker.funcResponse("Product with max price: ", Boolean.TRUE, temp.toString());
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            responseBody = dbFuncMinMax("max");
         } else if ("min".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-                    StringBuilder temp = new StringBuilder();
-                    while (rs.next()) {
-                        String name = rs.getString("name");
-                        int price = rs.getInt("price");
-                        temp.append(name).append("\t").append(price).append("</br>");
-                    }
-                    responseBody = ResponseMaker.funcResponse("Product with min price: ", Boolean.TRUE, temp.toString());
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            responseBody = dbFuncMinMax("min");
         } else if ("sum".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT SUM(price) FROM PRODUCT");
-                    String temp = "";
-                    if (rs.next()) {
-                        temp = ("" + rs.getInt(1));
-                    }
-                    responseBody = ResponseMaker.funcResponse("Summary price: \n", Boolean.FALSE, temp);
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            responseBody = dbFuncSumCount("sum");
         } else if ("count".equals(command)) {
-            try {
-                try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                    Statement stmt = c.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-                    String temp = "";
-                    if (rs.next()) {
-                        temp = ("" + rs.getInt(1));
-                    }
-                    responseBody = ResponseMaker.funcResponse("Number of products: \n", Boolean.FALSE, temp);
-
-                    rs.close();
-                    stmt.close();
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            responseBody = dbFuncSumCount("count");
         } else {
             responseBody = "Unknown command: " + command;
         }
         ResponseMaker.okResponse(response, responseBody);
     }
-
 }
